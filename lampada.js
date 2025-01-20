0f70e97b5177836a2ff39c832a22a729a3b102e6
IgorGoro91 <igorysc@gmail.com>
2025-01-20 17:00:16 +0100

    add:correzione

--- lampada.js
+++ lampada.js
@@ -1,15 +1,10 @@
 
 
 const lampadina = document.getElementById(`lampadina`);
-const btn = document.getElementById(`btn`)
+const btn = document.getElementById(`btn`);
 
-btn.addEventListener(`click`,() => {
-    if(lampadina.src.includes(`white_lamp.png`)){
-        lampadina.src = `yellow_lamp.png`;
-        btn.textContent = `Spegni`;
-    }else{
-        lampadina.src = `white_lamp.png`;
-        btn.textContent = `Accendi`
-    }
+btn.addEventListener(`click`, function(){
+    console.log(lampadina);
+    lampadina.src = "./img-5/yellow_lamp.png";
 })
 
